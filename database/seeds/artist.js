583d01280536a0a2c7c504b5846a4e54f598c32b
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-03-24 23:21:26 +0500

    First Commit

--- database/seeds/artist.js
+++ database/seeds/artist.js
@@ -1,6 +1,6 @@
-import _ from 'lodash';
-import faker from 'faker';
-import { GENRES } from '../constants';
+import _ from 'lodash'
+import faker from 'faker'
+import { GENRES } from '../constants'
 
 module.exports = function() {
   return {
@@ -15,12 +15,12 @@ module.exports = function() {
     labelName: faker.company.companyName(),
     retired: faker.random.boolean(),
     albums: getAlbums()
-  };
+  }
 }
 
 function getAlbums() {
   return _.times(randomBetween(0, 5), () => {
-    const copiesSold = randomBetween(0, 1000000);
+    const copiesSold = randomBetween(0, 1000000)
 
     return {
       title: _.capitalize(faker.random.words()),
@@ -29,25 +29,25 @@ function getAlbums() {
       numberTracks: randomBetween(1, 20),
       image: getAlbumImage(),
       revenue: copiesSold * 12.99
-    };
-  });
+    }
+  })
 }
 
 function getAlbumImage() {
-  const types = _.keys(faker.image);
-  const method = randomEntry(types);
+  const types = _.keys(faker.image)
+  const method = randomEntry(types)
 
-  return faker.image[method]();
+  return faker.image[method]()
 }
 
 function getGenre() {
-  return randomEntry(GENRES);
+  return randomEntry(GENRES)
 }
 
 function randomEntry(array) {
-  return array[~~(Math.random() * array.length)];
+  return array[~~(Math.random() * array.length)]
 }
 
 function randomBetween(min, max) {
-  return ~~(Math.random() * (max-min)) + min;
+  return ~~(Math.random() * (max - min)) + min
 }
